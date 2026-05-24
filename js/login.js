069b38b37ce92c9420c0d694be2c8f2889fcf87b
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-05-23 22:23:55 -0600

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,27 @@
+document.getElementById("loginForm")
+.addEventListener("submit", async(e)=>{
 
+e.preventDefault();
+
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+
+const response = await fetch(
+`${API_URL}?action=login&email=${email}&password=${password}`
+);
+
+const data = await response.json();
+
+if(data.status){
+
+localStorage.setItem("user", JSON.stringify(data.user));
+
+window.location = "dashboard.html";
+
+}else{
+
+alert(data.message);
+
+}
+
+});
