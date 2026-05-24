33ca858093e48478c7d7654c4dd5be2e4c8777ca
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-05-24 17:43:35 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbzOhe1cGuLafkptsHzrOmEGvefp8UUYnbbr9rwXUm6Ke2Ngmmy2iPpPHtjxeA_oD6DI/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxSirWbyFuMswlUOHc3hBJBmvkSWjm5ShQxlAGQbgxjvUW1tOkZ_wsmK94HdhHJRFuc/exec";
