c0fac70d124fc3545bbe2430a14d3df15d372bd6
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-05-24 00:24:38 -0600

    Update login.js

--- js/login.js
+++ js/login.js
@@ -3,24 +3,66 @@ document.getElementById("loginForm")
 
 e.preventDefault();
 
-const email = document.getElementById("email").value;
-const password = document.getElementById("password").value;
+const email =
+document.getElementById("email").value;
+
+const password =
+document.getElementById("password").value;
+
+try{
 
 const response = await fetch(
-`${API_URL}?action=login&email=${email}&password=${password}`
+
+`${API_URL}?action=login&email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`,
+
+{
+method:"GET"
+}
+
 );
 
 const data = await response.json();
 
 if(data.status){
 
-localStorage.setItem("user", JSON.stringify(data.user));
+localStorage.setItem(
+"user",
+JSON.stringify(data.user)
+);
 
-window.location = "dashboard.html";
+Swal.fire({
+
+icon:"success",
+title:"Bienvenido"
+
+}).then(()=>{
+
+window.location.href =
+"dashboard.html";
+
+});
 
 }else{
 
-alert(data.message);
+Swal.fire({
+
+icon:"error",
+title:data.message
+
+});
+
+}
+
+}catch(error){
+
+console.error(error);
+
+Swal.fire({
+
+icon:"error",
+title:"Error conexión Apps Script"
+
+});
 
 }
 
