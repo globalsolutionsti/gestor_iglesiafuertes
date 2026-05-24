e46c57e9ea049f8614d39a6f82ccb7660f48933a
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-05-23 22:25:45 -0600

    Update eventos.js

--- js/eventos.js
+++ js/eventos.js
@@ -1,1 +1,27 @@
+async function cargarEventos(){
 
+const response = await fetch(
+`${API_URL}?action=getEventos`
+);
+
+const data = await response.json();
+
+let html = "";
+
+for(let i=1; i<data.data.length; i++){
+
+html += `
+<div class="evento-card">
+<h4>${data.data[i][1]}</h4>
+<p>${data.data[i][3]}</p>
+</div>
+`;
+
+}
+
+document.getElementById("listaEventos")
+.innerHTML = html;
+
+}
+
+cargarEventos();
