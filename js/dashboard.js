c6ce420744b1b9e627407a1056fdfb3f731f857f
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-05-23 23:22:57 -0600

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,27 +1,49 @@
 async function loadDashboard(){
 
+mostrarLoader();
+
 const response = await fetch(
-`${API_URL}?action=getServidores`
+`${API_URL}?action=estadisticas`
 );
 
 const data = await response.json();
 
 document.getElementById("totalServidores")
-.innerText = data.data.length - 1;
+.innerText = data.totalServidores;
 
 const ctx = document.getElementById('chart');
 
 new Chart(ctx, {
-type: 'line',
-data: {
-labels: ['Lun','Mar','Mie','Jue','Vie'],
-datasets: [{
-label: 'Asistencias',
-data: [12,19,8,15,20],
-borderWidth: 3
+
+type:'bar',
+
+data:{
+
+labels:[
+'Servidores',
+'Eventos',
+'Asistencias'
+],
+
+datasets:[{
+
+label:'Sistema',
+
+data:[
+data.totalServidores,
+data.totalEventos,
+data.totalAsistencias
+],
+
+borderWidth:2
+
 }]
+
 }
+
 });
+
+ocultarLoader();
 
 }
 
