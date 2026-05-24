4d3ad5003251f94a1b744f9f425086c70c2fcd84
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-05-23 22:26:16 -0600

    Update asistencia.js

--- js/asistencia.js
+++ js/asistencia.js
@@ -1,1 +1,38 @@
+function onScanSuccess(decodedText){
 
+registrar(decodedText);
+
+}
+
+const html5QrCode = new Html5Qrcode("reader");
+
+Html5Qrcode.getCameras().then(devices => {
+
+html5QrCode.start(
+devices[0].id,
+{
+fps:10,
+qrbox:250
+},
+onScanSuccess
+);
+
+});
+
+async function registrar(numeroServidor){
+
+const response = await fetch(
+`${API_URL}?action=registrarAsistencia`,
+{
+method:"POST",
+body:JSON.stringify({
+servidorId:numeroServidor
+})
+}
+);
+
+const data = await response.json();
+
+alert(data.message);
+
+}
