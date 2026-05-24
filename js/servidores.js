9b69fb71a529e34a2dcdde4d6d0f251542b7411d
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-05-23 22:25:11 -0600

    Update servidores.js

--- js/servidores.js
+++ js/servidores.js
@@ -1,1 +1,34 @@
+async function cargarServidores(){
 
+const response = await fetch(
+`${API_URL}?action=getServidores`
+);
+
+const data = await response.json();
+
+let html = "";
+
+for(let i=1; i<data.data.length; i++){
+
+html += `
+<tr>
+<td>${data.data[i][0]}</td>
+<td>${data.data[i][2]}</td>
+<td>${data.data[i][9]}</td>
+</tr>
+`;
+
+}
+
+document.getElementById("tablaServidores")
+.innerHTML = html;
+
+}
+
+cargarServidores();
+
+function mostrarFormulario(){
+
+alert("Formulario siguiente etapa");
+
+}
