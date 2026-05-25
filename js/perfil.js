658a77130663bf2619d719f37916e3ebffb7a17a
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-05-24 20:03:35 -0600

    Update perfil.js

--- js/perfil.js
+++ js/perfil.js
@@ -1,31 +1,175 @@
-const params = new URLSearchParams(
+const params =
+new URLSearchParams(
 window.location.search
 );
 
-const numero = params.get("numero");
+const id = params.get("id");
 
-async function cargarPerfil(){
+cargarPerfil();
 
-const response = await fetch(
-`${API_URL}?action=getServidor&numero=${numero}`
-);
+function cargarPerfil(){
 
-const data = await response.json();
+mostrarLoader();
 
-const s = data.servidor;
+const callbackName =
+"jsonp_callback_" + Date.now();
 
-document.getElementById("nombrePerfil")
-.innerText = s[2] + " " + s[3];
+window[callbackName] = function(response){
 
-document.getElementById("ministerioPerfil")
-.innerText = s[9];
+const data = response.data;
 
-document.getElementById("fotoPerfil")
-.src = s[13];
+const servidor =
+data.find(s => s[1] == id);
 
-document.getElementById("qrPerfil")
-.src = s[14];
+if(!servidor){
+
+Swal.fire({
+
+icon:"error",
+title:"Servidor no encontrado"
+
+});
+
+return;
 
 }
 
-cargarPerfil();
+document.getElementById(
+"fotoServidor"
+).src = servidor[10];
+
+document.getElementById(
+"nombreServidor"
+).innerText =
+servidor[2] + " " + servidor[3];
+
+document.getElementById(
+"ministerioServidor"
+).innerText =
+servidor[6];
+
+document.getElementById(
+"numeroServidor"
+).innerText =
+servidor[1];
+
+document.getElementById(
+"emailServidor"
+).innerText =
+servidor[5];
+
+document.getElementById(
+"telefonoServidor"
+).innerText =
+servidor[4];
+
+document.getElementById(
+"grupoServidor"
+).innerText =
+servidor[7];
+
+document.getElementById(
+"fechaIngreso"
+).innerText =
+servidor[8];
+
+// ASISTENCIAS DEMO
+
+document.getElementById(
+"historialAsistencias"
+).innerHTML = `
+
+<div class="timeline-item">
+
+<h6>Grupo Conexión</h6>
+
+<p>Asistencia registrada</p>
+
+</div>
+
+<div class="timeline-item">
+
+<h6>Evento General</h6>
+
+<p>Participó en reunión especial</p>
+
+</div>
+
+`;
+
+// FORMACIÓN DEMO
+
+document.getElementById(
+"historialFormacion"
+).innerHTML = `
+
+<div class="curso-card">
+
+<h6>Fundamentos</h6>
+
+<p>Completado</p>
+
+</div>
+
+<div class="curso-card">
+
+<h6>Liderazgo</h6>
+
+<p>En progreso</p>
+
+</div>
+
+`;
+
+ocultarLoader();
+
+document.body.removeChild(script);
+
+delete window[callbackName];
+
+};
+
+const script =
+document.createElement("script");
+
+script.src =
+
+`${API_URL}?action=getServidores&callback=${callbackName}`;
+
+document.body.appendChild(script);
+
+}
+
+function mostrarLoader(){
+
+document.body.insertAdjacentHTML(
+
+"beforeend",
+
+`
+
+<div class="loader-overlay"
+id="loader">
+
+<div class="loader"></div>
+
+</div>
+
+`
+
+);
+
+}
+
+function ocultarLoader(){
+
+const loader =
+document.getElementById("loader");
+
+if(loader){
+
+loader.remove();
+
+}
+
+}
